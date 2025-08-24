2d7d9d916d6f8a4ff45d9c2c9a730dd709259a86
barrybyte <abhayraj.12667@gmail.com>
2025-08-24 17:27:38 +0530

    refactored ex01

--- SOLID/ex01/README.md
+++ SOLID/ex01/README.md
@@ -3,7 +3,7 @@
 How to run:
 ```bash
 cd src
-javac Demo01.java.java
+javac Demo01.java
 java Demo01
 ```
 
@@ -11,3 +11,9 @@ Tasks:
 - Identify the SOLID violation(s)
 - Refactor to comply with all SOLID principles
 - Keep behavior; add a tiny demo/test
+
+
+Observations : 
+1. new object EmailClient is created in the orderService. -> violating Dependency Inversion Principle -> create an interface and initialize a new object in the Driver code (main file).
+
+2. Order service is doing too many things 
--- SOLID/ex01/src/OrderService.java
+++ SOLID/ex01/src/OrderService.java
@@ -1,13 +1,18 @@
 public class OrderService {
     double taxRate = 0.18;
-    EmailClient email = new EmailClient();
+    // EmailClient email = new EmailClient();
+    INotificationSender notificationSender;
+
+    public OrderService(INotificationSender notificationSender){
+        this.notificationSender = notificationSender; 
+    }
 
     double totalWithTax(double subtotal) {
         return subtotal + subtotal * taxRate;
     }
     void checkout(String customerEmail, double subtotal) {
         double total = totalWithTax(subtotal);
-        email.send(customerEmail, "Thanks! Your total is " + total);
+        notificationSender.send(customerEmail, "Thanks! Your total is " + total);
         System.out.println("Order stored (pretend DB).");
     }
 }
